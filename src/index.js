69b2ee5796e7e3b0be2fb832800cc133dd166d2c
mike <mike@espirus.com>
2021-08-03 12:15:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,9 +51,9 @@ class AsyncPaginationExample extends React.Component {
               alt={option.login}
               src={option.avatar_url}
               style={{
-                height: '60px',
+                height: '80px',
                 marginRight: '20px',
-                width: '60px',
+                width: '80px',
               }}
             />
             <span>{option.login}</span>
